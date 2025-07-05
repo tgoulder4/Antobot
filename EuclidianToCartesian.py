3d0072ee3c96242049fcb820b0da2122d7e97afc
Tye <tcjgoulder@gmail.com>
2025-07-05 16:54:06 +0100

    initial

---
+++ EuclidianToCartesian.py
@@ -0,0 +1,32 @@
+
+#converting GPS to cartesian. will be done 
+from math import cos,sin
+import matlab.engine
+
+#model earth as a perfect sphere
+# take GPS lat long, set as 0,0 (local co-ord system)
+
+R=6.378e06 #radius of the earth
+def useTrigonometric(lat,lon):
+        x=R*cos(lat)*sin(lon)
+        y=R*cos(lat)*cos(lon)
+        z=R*cos(lon)
+        return (x,y,z)
+
+def useGeodetic(lat,lon):
+      #call the geodeticToECEF fn in matlab
+      #initialisation
+      eng = matlab.engine.start_matlab()
+      cart = eng.geodetic2ecef()
+      eng.quit()
+      return cart
+      
+def convertEuclidianLatLonToCartesian(lat,lon):
+      print("Converting lat "+str(lat) + " and long "+str(lon)+ " to Cartesian")
+      # we could convert from polar co-ordinates to cartesian using simple trigonometry x=rcostheta y=rsintheta 
+      cartesian = useTrigonometric(lat,lon)
+      # cartesian = useGeodetic(lat,lon)
+      print("----------> Resulting cartesian co-ord: "+str(cartesian))
+
+      return cartesian
+      # convert this euclidian to cartestian
---
+++ main.py
@@ -0,0 +1,65 @@
+from EuclidianToCartesian import convertEuclidianLatLonToCartesian
+import time
+
+def getLatLonFromGPS():
+    return {'lat':120,'lon':150}
+
+def getECEF():
+    latLon = getLatLonFromGPS()
+    xyz=convertEuclidianLatLonToCartesian(latLon['lat'],latLon['lon'])
+    print('returning xyz: '+str(xyz))
+    return {'x':xyz[0],'y':xyz[1],'z':xyz[2]}
+
+def getCoveredBounds(origin):
+    print("Getting bounds relative to origin...")
+    time.sleep(2)
+    return {'xMax':200-origin['x'],'xMin':100-origin['x'],'yMax':100-origin['y'],'yMin':100-origin['y'],'zMax':1000-origin['z'],'zMmin':750-origin['z']}
+
+def checkWithinBounds(bounds,origin):
+    print("Checking the origin of summon is within bounds...")
+    reason=None
+    #check max
+    if (origin['x'] < bounds['xMin'] or origin['x'] > bounds['xMax']):
+        reason='x'
+        return reason
+    elif (origin['y'] < bounds['yMin'] or origin['y'] > bounds['yMax']):
+        reason = "y"
+        return reason
+
+    if reason:
+        return reason + " is out of bounds"
+    else:
+         return True
+
+def planPathAndAct(origin,target,staticMap):
+    print("Planning path...")
+    time.sleep(1) #mock implementation
+    print("Moving along path...")
+    time.sleep(1) #mock implementation
+    
+
+def main():
+    #on summon press
+    origin = getECEF()
+    bounds = getCoveredBounds(origin)
+    print("Origin is "+str(origin) + ", relative bounds are "+str(bounds))
+
+    isWithinBounds = checkWithinBounds(bounds,origin)
+    
+    if isWithinBounds==True:
+        #get current lat long
+        print("Position is within bounds. Getting current lat and long...")
+        time.sleep(2)
+        currentLatLong = [10,20]
+        print("Getting origin...")
+        origin=convertEuclidianLatLonToCartesian(currentLatLong[0],currentLatLong[1])
+
+        #takes origin, target, staticmap
+        planPathAndAct(origin,[None,None,None],'')
+        print('Finished!')
+    else:
+        print("Position is out of bounds!")
+
+main()
+        
+# needs trialling with mock co-ordinates.
